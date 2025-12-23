11fbb7bd150c6b49df62a8632ed207d070260aee
mkhshohud99 <mkhshohud1999@gmail.com>
2025-12-24 05:09:33 +0600

    Product added Successfully

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
-const app = express();  
+const app = express();
 app.use(cors())
 app.use(express())
+app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -26,6 +27,38 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const database = client.db('bookCurier_Library')
+    const userCollections = database.collection('user')
+    const booksCollections = database.collection('books')
+
+    app.post('/users', async (req, res) => {
+      const userInfo = req.body;
+      userInfo.createdAt = new Date();
+
+      const result = await userCollections.insertOne(userInfo);
+      res.send(result);
+    });
+
+
+    app.post('/books', async (req, res) => {
+      const bookInfo = req.body;
+      bookInfo.createdAt = new Date();
+      const result = await booksCollections.insertOne(bookInfo);
+      res.send(result);
+    });
+
+
+    app.get('/users/role/:email', async (req, res) => {
+      const { email } = req.params
+
+      const query = { email: email }
+      const result = await userCollections.findOne(query)
+      // console.log(result)
+      res.send(result)
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -36,11 +69,11 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
-    res.send("Hello Form BookCurier Library Server")
+app.get('/', (req, res) => {
+  res.send("Hello Form BookCurier Library Server")
 })
 
-app.listen(port, ()=>{
-    console.log(`Server is running on Port: ${port}`);
-    
+app.listen(port, () => {
+  console.log(`Server is running on Port: ${port}`);
+
 })
