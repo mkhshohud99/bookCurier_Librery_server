d6fe4b3c26d15c6cecafff8a8e93036d05262351
mkhshohud99 <mkhshohud1999@gmail.com>
2025-12-26 07:26:39 +0600

    completely removed service account key

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
@@ -10,7 +11,36 @@ app.use(express())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const admin = require("firebase-admin");
+// const serviceAccount = require("./firebase-admin-key.json");
+
+const decoded = Buffer.from(process.env.FB_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  console.log(token);
+  
+
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorize access' })
+  }
+  try {
+    
+    const decoded = await admin.auth().verifyIdToken(token)
+    console.log("decoded Information", decoded);
+    req.decoded_email = decoded.email;
+    next();
+
+  }
+  catch (error) {
+    return res.status(401).send({ message: 'Unauthorize access' })
+  }
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sctb0kd.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +61,7 @@ async function run() {
     const database = client.db('bookCurier_Library')
     const userCollections = database.collection('user')
     const booksCollections = database.collection('books')
+    const ordersCollections = database.collection('orders')
 
     app.post('/users', async (req, res) => {
       const userInfo = req.body;
@@ -57,6 +88,40 @@ async function run() {
       // console.log(result)
       res.send(result)
     })
+    app.get('/books/:email', async (req, res) => {
+      const { email } = req.params
+      const query = { email: email }
+      const result = await booksCollections.find(query).toArray()
+      //console.log(result)
+      res.send(result)
+    })
+    app.get('/orders/:email', async (req, res) => {
+      const { email } = req.params
+      const query = { email: email }
+      const result = await ordersCollections.find(query).toArray()
+      //console.log(result)
+      res.send(result)
+    })
+    app.get('/books', async (req, res) => {
+      const result = await booksCollections.find().toArray()
+      console.log(result)
+      res.send(result)
+    })
+    app.get('/books/id/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) }; // Use _id and wrap the string in ObjectId
+      const result = await booksCollections.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/orders', async(req,res)=>{
+      const orderInfo = req.body;
+      orderInfo.createdAt = new Date();
+      const result = await ordersCollections.insertOne(orderInfo);
+      res.send(result);
+    })
+
 
 
     await client.db("admin").command({ ping: 1 });
